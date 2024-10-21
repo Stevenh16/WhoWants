26f579a3bcea333a16c4d4101bf5051e8438529b
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-21 18:59:19 -0500

    [MOD]-> add username AND roles

--- src/main/java/model/entity/Person.java
+++ src/main/java/model/entity/Person.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
+import java.util.Set;
 
 @Builder
 @Setter
@@ -18,6 +19,8 @@ public class Person {
     private Long id;
     @Column(name = "name")
     private String name;
+    @Column(name = "username")
+    private String username;
     @Column(name = "email")
     private String email;
     @Column(name = "password")
@@ -26,4 +29,10 @@ public class Person {
     private List<Donation> donations;
     @OneToMany(mappedBy = "beneficiary",fetch = FetchType.LAZY)
     private List<Donation> benefits;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
 }
