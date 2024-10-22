e90a544b29d61b13f2289462967a5dbba833b9d4
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-21 19:08:34 -0500

    [MOD]-> add methods

--- src/main/java/model/repository/PersonRepository.java
+++ src/main/java/model/repository/PersonRepository.java
@@ -3,5 +3,11 @@ package model.repository;
 import model.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByUsername(String username);
+    Optional<Person> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
