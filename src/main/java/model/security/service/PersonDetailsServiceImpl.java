d991707927ecc2f40d83a9cdd3bda3296dcfe73f
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-10-21 19:10:22 -0500

    [CREATE]-> /security/service DetailsImpl, DetailsServicImpl

---
+++ src/main/java/model/security/service/PersonDetailsServiceImpl.java
@@ -0,0 +1,17 @@
+package model.security.service;
+
+import model.entity.Person;
+import model.repository.PersonRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class PersonDetailsServiceImpl implements UserDetailsService {
+    private PersonRepository personRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Person person = personRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return PersonDetailsImpl.build(person);
+    }
+}
