8815864f093a5791f9645dd1f99277662b68effa
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-11-12 19:58:16 -0500

    [FIX]-> starting correctly repository

--- src/main/java/model/security/service/PersonDetailsServiceImpl.java
+++ src/main/java/model/security/service/PersonDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package model.security.service;
 
+import lombok.AllArgsConstructor;
 import model.entity.Person;
 import model.repository.PersonRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,8 +9,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class PersonDetailsServiceImpl implements UserDetailsService {
-    private PersonRepository personRepository;
+    private final PersonRepository personRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Person person = personRepository.findByUsername(username)
