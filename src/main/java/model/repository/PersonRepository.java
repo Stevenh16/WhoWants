e61f7f8dc8962127b257abb285118a3fee82c420
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-11-12 20:00:58 -0500

    [FIX] add @Query

--- src/main/java/model/repository/PersonRepository.java
+++ src/main/java/model/repository/PersonRepository.java
@@ -2,11 +2,14 @@ package model.repository;
 
 import model.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    Optional<Person> findByUsername(String username);
+    @Query("SELECT p FROM Person p JOIN FETCH p.roles WHERE p.username = :username")
+    Optional<Person> findByUsername(@Param("username") String username);
     Optional<Person> findByEmail(String email);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
