455c92f4c80dbc15c1566019a10e99be97582189
Steven Navarro <sdnavarros@unimagdalena.edu.co>
2024-11-24 21:28:52 -0500

    [MOD]-> apliying changes done in ratingDto

--- src/main/java/model/mapper/RatingMapper.java
+++ src/main/java/model/mapper/RatingMapper.java
@@ -12,23 +12,29 @@ import java.util.List;
 public interface RatingMapper {
     @Named("complete")
     @Mapping(source = "rating.donation.id", target = "donation")
+    @Mapping(source = "rating.person.id", target = "person")
     RatingDto toIdDto(Rating rating);
     @Named("withoutId")
     @Mapping(source = "rating.donation.id", target = "donation")
+    @Mapping(source = "rating.person.id", target = "person")
     @Mapping(target = "id", ignore = true)
     RatingDto toDto(Rating rating);
     @Named("listComplete")
     @Mapping(source = "rating.donation.id", target = "donation")
+    @Mapping(source = "rating.person.id", target = "person")
     List<RatingDto> toListIdDto(List<Rating> ratings);
 
     @Named("listWithoutId")
     @Mapping(target = "id", ignore = true)
     @Mapping(source = "rating.donation.id", target = "donation")
+    @Mapping(source = "rating.person.id", target = "person")
     List<RatingDto> toListDto(List<Rating> ratings);
 
 
     @Mapping(source = "ratingDto.donation", target = "donation")
+    @Mapping(source = "ratingDto.person", target = "person")
     Rating toEntity(RatingDto ratingDto);
     @Mapping(source = "ratingDto.donation", target = "donation")
+    @Mapping(source = "ratingDto.person", target = "person")
     List<Rating> toListEntity(List<RatingDto> ratingDtos);
 }
